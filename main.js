c272ccbc0946cb3907a4b62e7506f9a785dcd07a
utsab043 <grgutsab66@gmail.com>
2024-02-03 23:57:39 +0545

    WIP:Click action added

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-//players div
-const playerDOM = document.querySelector('#player');
+const Cell = document.querySelectorAll('.cell');
+
+turn0 = true;
+
+
+Cell.forEach((cell) => {
+  cell.addEventListener("click", () => {
+    if (turn0) {
+      cell.innerText = "X";
+      turn0 = false;
+    } else {
+      cell.innerText = "O";
+      turn0 = true;
+    }
+  })
+})
